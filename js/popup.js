3c5ee43fdb6025bd8961e8c3fc32c0736989c966
AJ Nettles <ajnettles@gmail.com>
2023-10-17 00:36:02 -0500

    Working version, pre-clean

--- js/popup.js
+++ js/popup.js
@@ -170,35 +170,48 @@ function mercuryRebuild(url){
         
         let hostElem = document.getElementById("dlgGVInnerDialog").children[0].children[0].children[0].children[1].innerText
         building += " " + document.getElementById("BAECRConferenceBookingDisplay").innerText.split(":")[0].substring(3)
-        let checkOutNote = "overnight"
-        // let checkOutNote = document.getElementById("txtdlgGVCheckOutNote").value
+        // let checkOutNote = "overnight"
+        let checkOutNote = document.getElementById("txtdlgGVCheckOutNote").value
 
+        let checkInDate = document.getElementById("dlgGVCheckIn").value.split(" ")[0]
         let checkOutDate = document.getElementById("dlgGVCheckOut").value.split(" ")[0]
+        let checkInTime = (document.getElementById("dlgGVCheckIn").value.split(" ")[1] + " " + document.getElementById("dlgGVCheckIn").value.split(" ")[2]).replace(":","%3A").replace(" ", "%20")
         let checkOutTime = (document.getElementById("dlgGVCheckOut").value.split(" ")[1] + " " + document.getElementById("dlgGVCheckOut").value.split(" ")[2]).replace(":","%3A").replace(" ", "%20")
         let month = checkOutDate.split("/")[0]
+        if (month.length == 1) month = "0"+month
         let day = checkOutDate.split("/")[1]
+        if (day.length == 1) day = "0"+day
         let year = checkOutDate.split("/")[2]
         let date = year+"%2D"+month+"%2D"+day
         let template = ""
-        let name = hostElem.split(", ")[1].split(" ")[0] + " " + hostElem.split(",")[0]
+        let firstName = hostElem.split(", ")[1].split(" ")[0]
+        let lastName = hostElem.split(",")[0]
+        let guestFirstName = document.getElementById("DisplayText").value.split(", ")[1].split(" ")[0]
+        let guestLastName = document.getElementById("DisplayText").value.split(",")[0]
+        let roomNumber = document.getElementById("BAECRConferenceBookingDisplay").innerText.split(":")[0]
+        let name = firstName + " " + lastName
+        let guestName = guestFirstName + " " + guestLastName
         if (checkOutNote.toLowerCase().includes("improper")) {
+            template = `At approximately ${checkInTime} on ${checkInDate}, ${building.split(" ",2).join(" ")} resident, ${name}, signed in guest, ${guestName}, to room ${roomNumber}. ${lastName} did not sign out their guest. This constitutes an improper checkout violation. `
             violationType = "improper"
             console.log(violationType)
         }else if(checkOutNote.toLowerCase().includes("overnight")){
             // TODO: Finish templates for each type of situation
-            template = `At approximately ${checkOutTime} on ${date}, Gold Hall resident, ${name}, signed in guest, NAME, to room RESIDENTROOMNUMBER. Resident RESIDENTLASTNAME signed out their guest at TIME on DATE and did not have an overnight guest form on file. This constitutes an overnight guest violation.`.replace(" ", "%20").replace(",", "%2C")
+            template = `At approximately ${checkInTime} on ${checkInDate}, ${building.split(" ",2).join(" ")} resident, ${name}, signed in guest, ${guestName}, to room ${roomNumber}. Resident ${lastName} signed out their guest at ${checkOutTime} on ${checkOutDate} and did not have an overnight guest form on file. This constitutes an overnight guest violation.`.replace(" ", "%20").replace(",", "%2C")
             violationType = "overnight"
             console.log(violationType)
         }else if(checkOutNote.toLowerCase().includes("unescorted")){
+            template = ``
             violationType = "unescorted"
             console.log(violationType)
         }
         newElem.onclick = function(){
-            // link = `https://cm.maxient.com/reportingform.php?UnivofAlabamaBirmingham&layout_id=0&reporters_full_name=${hostElem.split(",")[0]}`
+            // link = `https://cm.maxient.com/reportingform.php?UnivofAlabamaBirmingham&layout_id=0&reporters_full_name=${lastName}`
             console.log(building.split(" ",2).join(" ").replace(" ", "%20"))
-            newElem.href = `https://cm.maxient.com/reportingform.php?UnivofAlabamaBirmingham&layout_id=0&location_of_incident_specific=${building.replace(" ", "%20")}&location_of_incident=${building.split(" ",2).join(" ").replace(" ", "%20")}&search_firstname=${hostElem.split(", ")[1].split(" ")[0]}&search_lastname=${hostElem.split(",")[0]}&search_housing_room_num=${parseInt(document.getElementById("BAECRConferenceBookingDisplay").innerText.split(":")[0].substring(3))}`
+            newElem.href = `https://cm.maxient.com/reportingform.php?UnivofAlabamaBirmingham&layout_id=0&location_of_incident_specific=${building.replace(" ", "%20")}&location_of_incident=${building.split(" ",2).join(" ").replace(" ", "%20")}&search_firstname=${firstName}&search_lastname=${lastName}&search_housing_room_num=${parseInt(document.getElementById("BAECRConferenceBookingDisplay").innerText.split(":")[0].substring(3))}`
             console.log(hostElem)
         }
+        console.log(date)
         link = `https://cm.maxient.com/reportingform.php?UnivofAlabamaBirmingham&layout_id=0&location_of_incident_specific=${building.replace(" ", "%20")}&location_of_incident=${building.split(" ",2).join(" ").replace(" ", "%20")}&search_firstname=${hostElem.split(", ")[1].split(" ")[0]}&search_lastname=${hostElem.split(",")[0]}&search_housing_room_num=${parseInt(document.getElementById("BAECRConferenceBookingDisplay").innerText.split(":")[0].substring(3))}&date_of_incident=${date}&time_of_incident=${checkOutTime}&aq[1][answer]=${template}`
         // chrome.tabs.create({ url: link, active: false });
         chrome.runtime.sendMessage({ link });
@@ -365,7 +378,10 @@ function excelExtract(rowNumber){
         default:
             rAddress.value = "1720 University Blvd, Birmingham, AL 35205";
     }
-
+    let role = document.getElementById("role_0");
+    if (role.value == "" || role.value == null){
+        role.value = "Alleged (Student)";
+    }
 
     console.log("[MAVI] Autofill complete!");
 }
